f7a1ae69b7671829baecab8fa3463688f9f9615b
Serhat Özdemir <serhat.oezd@web.de>
2026-04-09 03:53:17 +0200

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const year = new Date().getFullYear();
+
+function Footer() {
+    return (
+
+        <footer>
+            <p>
+                Copyright ⓒ {year}
+            </p>
+        </footer>
+    )
+}
+
+
+export default Footer;
